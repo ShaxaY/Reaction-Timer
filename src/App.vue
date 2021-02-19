b84d9960867e4b169a0b42137114d20fc49cd2b1
Shakhzod Yokubov <shakhzod.y@gmail.com>
2021-02-19 14:17:00 +0500

    feat: implement results component

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p v-if="showResults">Reaction time: {{ score }} ms</p>
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
@@ -41,5 +42,24 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background-color: #0faf87;
+  color: #fff;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 8px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+  transition: all 0.5s;
+}
+button:hover {
+  transform: translateY(-3px);
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
